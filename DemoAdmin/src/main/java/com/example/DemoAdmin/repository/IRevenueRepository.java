911fac085db7c3f8fa7f1744c233ba016612d317
Nhaan2003 <hoainhan2904@gmai.com>
2025-06-05 08:41:45 +0700

    Chỉnh phần doanh thu

--- DemoAdmin/src/main/java/com/example/DemoAdmin/repository/IRevenueRepository.java
+++ DemoAdmin/src/main/java/com/example/DemoAdmin/repository/IRevenueRepository.java
@@ -17,18 +17,16 @@ public interface IRevenueRepository extends JpaRepository<BookingSeat, Integer> 
     @Query(value = """
         SELECT 
             CONCAT(m.Title, ' - ', t.Name) AS label,
-            SUM(sp.Price) AS amount
-        FROM BookingSeat bs
-        JOIN Booking b ON bs.BookingId = b.BookingId
+            SUM(b.Total) AS amount
+        FROM Booking b
         JOIN Showtime s ON b.ShowtimeId = s.ShowtimeId
         JOIN Movie m ON s.MovieId = m.MovieId
         JOIN Screen sc ON s.ScreenId = sc.ScreenId
         JOIN Theater t ON sc.TheaterId = t.TheaterId
-        JOIN Seat seat ON bs.SeatId = seat.SeatId
-        JOIN SeatPrice sp ON sp.SeatTypeId = seat.SeatTypeId AND sp.ScreenId = sc.ScreenId
         WHERE b.BookingDate BETWEEN :startDate AND :endDate
           AND (:theaterId IS NULL OR t.TheaterId = :theaterId)
           AND (:movieId IS NULL OR m.MovieId = :movieId)
+          AND b.Status = 'Success'
         GROUP BY m.Title, t.Name
     """, nativeQuery = true)
     List<Object[]> findRevenueByAllNative(
@@ -37,4 +35,5 @@ public interface IRevenueRepository extends JpaRepository<BookingSeat, Integer> 
             @Param("theaterId") Integer theaterId,
             @Param("movieId") Integer movieId
     );
+
 }
